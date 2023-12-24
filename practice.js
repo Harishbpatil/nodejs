8dc919eb02353661e5a8dee7267a3404153f6f2c
Harishbpatil <hp777489@gmail.com>
2023-12-24 17:28:19 +0530

    based on url /home and /node and /about request response

---
+++ practice.js
@@ -0,0 +1,11 @@
+//   const url = req.url;
+//   if (url === "/") {
+//     res.write("<html>");
+//     res.write("<head><title>Enter Message</title>");
+//     res.write(
+//       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+//     );
+//     res.write("</html>");
+//     res.end();
+//   }
+// });
