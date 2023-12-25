a7ad5f06e3c54620b4b7543281480f1383d697d0
Harishbpatil <hp777489@gmail.com>
2023-12-25 16:43:30 +0530

    create route and export  clean up code

---
+++ routes.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const requestHandler = (req, res) => {
+  if (req.url === "/") {
+    fs.readFile("message.txt", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        return res.end("Internal Server Error");
+      }
+
+      let result =
+        "<html><head><title>Harish</title></head>" +
+        "<body><p>" +
+        data +
+        '<form action="/message" method="POST"><input type="text" name="message"/> <button type="submit" >send</button> </form></body></html>';
+      return res.end(result);
+    });
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        if (err) console.log(err);
+      });
+      res.statusCode = 302;
+      res.setHeader("location", "/");
+      return res.end();
+    });
+  }
+};
+
+module.exports = requestHandler;
+
+// module.exports.requestHandler;
+// module.exports.someText='some hard coded text';
+
+// exports.requestHandler;
+// exports.someText='some hard coded text';
+
+// module.exports={
+//     handler:requestHandler,
+//     someText:"some"
+// }
--- server.js
+++ server.js
@@ -1,47 +1,9 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes").default;
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
+const server = http.createServer(routes);
 
-    const message = fs.readFileSync("message.txt", "utf8");
-
-    res.write("<body>");
-
-    res.write("<p> " + message + "</p>");
-
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text" name="message" value="' + message + '">');
-    res.write('<button type="submit">Send</button>');
-    res.write("</form>");
-
-    res.write("</body>");
-    res.write("</html>");
-
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
-    });
-  }
-});
+// const server = http.createServer(routes.handler);
 
 server.listen(4000);
