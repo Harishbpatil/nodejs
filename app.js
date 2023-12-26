c9a6fd00a8d6f2c93491af6063ad75085f6e5dbf
Harishbpatil <hp777489@gmail.com>
2023-12-26 14:45:07 +0530

    express,use middlewares task 1

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = require("http");
-const routes = require("./routes");
 
-const server = http.createServer(routes);
+const express = require("express");
 
-server.listen(4000);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); //Allows the request to continue to next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("In other middleware!");
+  res.send("<h1>Hello From Express!!</h1>");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
