1cf9dfe24309e5fd16bc3b4ca8cf30083deca7c5
Harishbpatil <hp777489@gmail.com>
2023-12-26 17:18:19 +0530

    create add product route using bodyparser and add title and size inputs task 2 express

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //Allows the request to continue to next middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In other middleware!");
+app.post("/product", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello From Express!!</h1>");
 });
 
 const server = http.createServer(app);
-
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000 'http://localhost:3000/'");
+});
